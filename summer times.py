d2b04f822d2ed754e68a68b9f6075ea581f91f78
ravicbade-jpg <ravicbade@gmail.com>
2025-12-14 16:36:07 +0000

    summertimes.py

---
+++ summer times.py
@@ -0,0 +1,6 @@
+temperature = int(input("Enter the temperature in °C: "))
+
+if temperature >= 20:
+    print("You can wear light and soft clothes.")
+else:
+    print("It is cold. Wear a jacket or pullover.")
